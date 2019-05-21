6704a4edb33b6698bd1d93796900d6e5c221a46d
Laumania <mads@laumania.net>
2019-05-21 20:55:54 +0200

    Switched to .NET Core site - initial commit

---
+++ Source/WindowsFanDK-Replacement/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WindowsFanDK_Replacement.Models;
+
+namespace WindowsFanDK_Replacement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 60 * 60 * 24 * 7, Location = ResponseCacheLocation.Any)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Source/WindowsFanDK-Replacement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WindowsFanDK_Replacement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
